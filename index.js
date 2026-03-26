71d3d76f393f3275a483f1e22674a9bd918eded5
Ryan-Wandke <ryanwandke@gmail.com>
2026-03-26 11:57:05 -0500

    ryan trying to test nodejs

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ app = express()
 const cors = require("cors")
 
 var url = require('url');
-var dt = require('./date-time');
 
 const port = process.env.PORT || 3000
 
@@ -12,3 +11,30 @@ app.use(express.static(__dirname + '/static'))
 app.use(cors({ origin: '*' }))
 
 console.log("Hello world");
+
+// Ryan's nodejs test
+app.get('/test', (request, response) => {
+	console.log('Calling "/test" on the Node.js server.')
+	response.type('text/plain')
+	response.send('This is the test response.')
+})
+
+// Custom 404 page.
+app.use((request, response) => {
+  response.type('text/plain')
+  response.status(404)
+  response.send('404 - Not Found')
+})
+
+// Custom 500 page.
+app.use((err, request, response, next) => {
+  console.error(err.message)
+  response.type('text/plain')
+  response.status(500)
+  response.send('500 - Server Error')
+})
+
+app.listen(port, () => console.log(
+  `Express started at \"http://localhost:${port}\"\n` +
+  `press Ctrl-C to terminate.`)
+)
