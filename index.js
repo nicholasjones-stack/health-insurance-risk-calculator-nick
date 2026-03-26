ecd272d89a7ee7feea6b29c82e2b660373e156ec
Nick Jones <nick377@att.net>
2026-03-26 10:40:27 -0500

    UpdateForRyan

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express')
+app = express()
+
+const cors = require("cors")
+
+var url = require('url');
+var dt = require('./date-time');
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/static'))
+app.use(cors({ origin: '*' }))
+
 console.log("Hello world");
